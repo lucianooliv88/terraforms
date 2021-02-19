a5ba06235caef5401c8c0879ac7b90262847cb95
lucianooliv88 <44204746+lucianooliv88@users.noreply.github.com>
2021-02-19 12:13:55 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "terraform_wapp" {
+resource "aws_instance" "terraform_tests" {
     ami = "ami-0fc970315c2d38f01"
     instance_type = "t2.micro"
     vpc_security_group_ids =  [ "${aws_security_group.terraform_public_sg.id}" ]
