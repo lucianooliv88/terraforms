baed88fd95ff7317a604f7fab73a4853a558fad3
lucianooliv88 <44204746+lucianooliv88@users.noreply.github.com>
2021-02-19 14:51:37 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-output "instance_id_list"     { value = ["${aws_instance.terraform_tests.*.id}"] }
+output "instance_id_list" { 
+  value = ["${aws_instance.terraform_tests.*.id}"] }
+
+output "aws_vpc_id" {
+  value = "${aws_vpc.terraform-vpc.id}"
+}
+
