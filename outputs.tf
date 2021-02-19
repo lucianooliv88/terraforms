a576f234d4c3eec6ebcba18eb35712bf7bd5eb57
lucianooliv88 <44204746+lucianooliv88@users.noreply.github.com>
2021-02-19 16:57:06 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,5 @@
-output "instance_id_list" { 
-  value = ["${aws_instance.terraform_tests.*.id}"] }
+output "instance_id_list" { value = ["${aws_instance.terraform_tests.*.id}"] }
 
-output "aws_vpc_id" {
-  value = "${aws_vpc.terraform-vpc.id}"
-}
+output "aws_vpc_id" { value = "${aws_vpc.terraform-vpc.id}" }
 
-output "aws_subnet_subnet_1" {
-  value = "${aws_subnet.terraform-subnet_1.id}"
-}
+output "aws_subnet_subnet_1" { value = "${aws_subnet.terraform-subnet_1.id}" }
