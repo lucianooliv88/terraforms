456c6e81d022aadf1e0f8bb58454a7c1f1a7a411
lucianooliv88 <44204746+lucianooliv88@users.noreply.github.com>
2021-08-12 13:04:30 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform_tests" {
-    ami = "ami-0d26eb3972b7f8c96"
+    ami = "ami-02b4e72b17337d6c1"
     instance_type = "t2.micro"
     vpc_security_group_ids =  [ "${aws_security_group.terraform_public_sg.id}" ]
     subnet_id = "${aws_subnet.terraform-subnet_1.id}"
